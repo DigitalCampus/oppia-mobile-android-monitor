ac70ad9431316a144c84653f261536cc6f62806e
alexlittle <alex@alexlittle.net>
2013-10-16 14:11:08 +0200

    Add libraries

--- src/org/digitalcampus/oppia/monitor/activity/MonitorActivity.java
+++ src/org/digitalcampus/oppia/monitor/activity/MonitorActivity.java
@@ -18,17 +18,35 @@
 package org.digitalcampus.oppia.monitor.activity;
 
 import org.digitalcampus.oppia.monitor.R;
+import org.digitalcampus.oppia.monitor.application.OppiaMonitor;
 
+import com.bugsense.trace.BugSenseHandler;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
-import android.app.Activity;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 
-public class MonitorActivity extends AppActivity {
+public class MonitorActivity extends AppActivity implements OnSharedPreferenceChangeListener{
 
+	public static final String TAG = MonitorActivity.class.getSimpleName();
+	private SharedPreferences prefs;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		BugSenseHandler.initAndStartSession(this, OppiaMonitor.BUGSENSE_API_KEY);
 		setContentView(R.layout.activity_main);
+		
+		prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		prefs.registerOnSharedPreferenceChangeListener(this);
+		PreferenceManager.setDefaultValues(this, R.xml.prefs, false);
+		
 		this.drawHeader();
 	}
 
@@ -37,6 +55,32 @@ public class MonitorActivity extends AppActivity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+		switch (item.getItemId()) {
+			case R.id.menu_settings:
+				Intent i = new Intent(this, PrefsActivity.class);
+				startActivity(i);
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
+	
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+		Log.d(TAG, key + " changed");
+		if(key.equalsIgnoreCase(getString(R.string.prefs_server))){
+			Editor editor = sharedPreferences.edit();
+			if(!sharedPreferences.getString(getString(R.string.prefs_server), "").endsWith("/")){
+				String newServer = sharedPreferences.getString(getString(R.string.prefs_server), "")+"/";
+				editor.putString(getString(R.string.prefs_server), newServer);
+		    	editor.commit();
+			}
+		}
+		super.onSharedPreferenceChanged(sharedPreferences, key);
 	}
 
 }
